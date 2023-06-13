09d75a5ceb771adab28ec56b433fdbf55f8d7e7c
RyoIHA <ihara@cysec.cs.ritsumei.ac.jp>
2023-06-14 04:27:56 +0900

    Correction in progress

--- ApacheSetup.sh
+++ ApacheSetup.sh
@@ -4,8 +4,8 @@ sudo apt-get -y upgrade
 sudo apt-get install -y build-essential libpcre3 libpcre3-dev libssl-dev libexpat1-dev
 cd ~
 wget https://archive.apache.org/dist/httpd/httpd-2.4.49.tar.gz
-wget https://archive.apache.org/dist/apr/apr-1.7.0.tar.gz
-wget https://archive.apache.org/dist/apr/apr-util-1.6.1.tar.gz
+wget https://archive.apache.org/dist/apr/apr-1.7.4.tar.gz
+wget https://archive.apache.org/dist/apr/apr-util-1.6.3.tar.gz
 tar xvf httpd-2.4.49.tar.gz
 tar xvf apr-1.7.0.tar.gz
 tar xvf apr-util-1.6.1.tar.gz
