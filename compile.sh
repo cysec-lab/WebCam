258e8d9f880d895d8dcbb7086b866454285e4588
Ryosuke IHARA <124863136+RyoIHA@users.noreply.github.com>
2023-06-29 16:33:40 +0900

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ cd /usr/local/apache2/cgi-bin
 gcc capture.c -o capture.cgi
 gcc list.c -o list.cgi
 gcc rename.c -o rename.cgi
-gcc takepicture.c -o takepicture.cgi
+gcc takepicture.c -o takepicture.cgi -lcurl
 gcc update.c -o update.cgi
