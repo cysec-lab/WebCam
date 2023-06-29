1927977e75496e88e11890d916bf3c1b64b90180
Ryosuke IHARA <124863136+RyoIHA@users.noreply.github.com>
2023-06-29 16:27:05 +0900

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -4,7 +4,9 @@ cd WebCam
 sudo rm /usr/local/apache2/htdocs/*
 sudo rm /usr/local/apache2/cgi-bin/*
 sudo mkdir /usr/local/apache2/htdocs/images
+sudo mkdir /usr/local/apache2/htdocs/images2
 sudo chown daemon:daemon /usr/local/apache2/htdocs/images
+sudo chown daemon:daemon /usr/local/apache2/htdocs/images2
 sudo chmod +w /usr/local/apache2/htdocs
 sudo mv index.html /usr/local/apache2/htdocs
 sudo mv album.html /usr/local/apache2/htdocs
