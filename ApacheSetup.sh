662b5d8ba6323fc842a6d5ecaecbaeb27d16c5e9
Ryosuke IHARA <124863136+RyoIHA@users.noreply.github.com>
2023-06-30 12:16:39 +0900

    Update ApacheSetup.sh

--- ApacheSetup.sh
+++ ApacheSetup.sh
@@ -17,3 +17,26 @@ cd ~/httpd-2.4.49
 LDFLAGS=-latomic ./configure --prefix=/usr/local/apache2 
 sudo make
 sudo make install
+
+cd ..
+cd WebCam
+sudo rm /usr/local/apache2/htdocs/*
+sudo rm /usr/local/apache2/cgi-bin/*
+sudo mkdir /usr/local/apache2/htdocs/images
+sudo mkdir /usr/local/apache2/htdocs/images2
+sudo mkdir /usr/local/apache2/downloads
+sudo chown daemon:daemon /usr/local/apache2/htdocs/images
+sudo chown daemon:daemon /usr/local/apache2/htdocs/images2
+sudo chown daemon:daemon /usr/local/apache2/downloads
+sudo chmod +w /usr/local/apache2/htdocs
+sudo mv index.html /usr/local/apache2/htdocs
+sudo mv album.html /usr/local/apache2/htdocs
+sudo mv style.css /usr/local/apache2/htdocs
+sudo mv script.js /usr/local/apache2/htdocs
+sudo mv capture.c /usr/local/apache2/cgi-bin
+sudo mv list.c /usr/local/apache2/cgi-bin
+sudo mv rename.c /usr/local/apache2/cgi-bin
+sudo mv takepicture.c /usr/local/apache2/cgi-bin
+sudo mv update.c /usr/local/apache2/cgi-bin
+echo "" > /usr/local/apache2/cgi-bin/target.txt
+sudo usermod -a -G video daemon
