1b9549287392f3c83a1c8834d0e4b505164ba304
Ryosuke IHARA <124863136+RyoIHA@users.noreply.github.com>
2023-06-30 12:15:21 +0900

    Update ApacheSetup.sh

--- ApacheSetup.sh
+++ ApacheSetup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y build-essential libpcre3 libpcre3-dev libssl-dev libexpat1-dev libcurl4-openssl-dev
+#Apache2.4.49インストール
 cd ~
 wget https://archive.apache.org/dist/httpd/httpd-2.4.49.tar.gz
 wget https://archive.apache.org/dist/apr/apr-1.7.4.tar.gz
@@ -15,4 +17,3 @@ cd ~/httpd-2.4.49
 LDFLAGS=-latomic ./configure --prefix=/usr/local/apache2 
 sudo make
 sudo make install
-/usr/local/apache2/bin/httpd -v
