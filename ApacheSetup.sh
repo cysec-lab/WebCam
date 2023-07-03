1cd44155912a60027a9fe0fefcbee1d4fbe68be0
Ryosuke IHARA <124863136+RyoIHA@users.noreply.github.com>
2023-07-03 16:57:30 +0900

    Update ApacheSetup.sh

--- ApacheSetup.sh
+++ ApacheSetup.sh
@@ -16,34 +16,3 @@ cd ~/httpd-2.4.49
 LDFLAGS=-latomic ./configure --prefix=/usr/local/apache2
 sudo make
 sudo make install
-#Apache詳細
-cd ..
-cd WebCam
-sudo rm /usr/local/apache2/htdocs/*
-sudo rm /usr/local/apache2/cgi-bin/*
-sudo mkdir /usr/local/apache2/htdocs/images
-sudo mkdir /usr/local/apache2/htdocs/images2
-sudo mkdir /usr/local/apache2/downloads
-sudo chown daemon:daemon /usr/local/apache2/htdocs/images
-sudo chown daemon:daemon /usr/local/apache2/htdocs/images2
-sudo chown daemon:daemon /usr/local/apache2/downloads
-sudo chmod +w /usr/local/apache2/htdocs
-sudo cp index.html /usr/local/apache2/htdocs
-sudo cp album.html /usr/local/apache2/htdocs
-sudo cp style.css /usr/local/apache2/htdocs
-sudo cp script.js /usr/local/apache2/htdocs
-sudo cp capture.c /usr/local/apache2/cgi-bin
-sudo cp list.c /usr/local/apache2/cgi-bin
-sudo cp rename.c /usr/local/apache2/cgi-bin
-sudo cp takepicture.c /usr/local/apache2/cgi-bin
-sudo cp update.c /usr/local/apache2/cgi-bin
-echo "" > /usr/local/apache2/cgi-bin/target.txt
-sudo usermod -a -G video daemon
-#コンパイル
-cd ..
-cd /usr/local/apache2/cgi-bin
-gcc capture.c -o capture.cgi
-gcc list.c -o list.cgi
-gcc rename.c -o rename.cgi
-gcc takepicture.c -o takepicture.cgi -lcurl
-gcc update.c -o update.cgi
