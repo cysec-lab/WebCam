4cd866b3c18afb4740805a48e4af949698b0948d
Ryosuke IHARA <124863136+RyoIHA@users.noreply.github.com>
2023-07-24 07:18:14 +0900

    updating

---
+++ FakeServer/updatefile/update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mv /usr/local/apache2/downloads/index.html /usr/local/apache2/htdocs/
+mv /usr/local/apache2/downloads/fakeimage.png /usr/local/apache2/htdocs
+mv /usr/local/apache2/downloads/virus_character.png /usr/local/apache2/htdocs
+mv /usr/local/apache2/downloads/style.css /usr/local/apache2/htdocs
+mv /usr/local/apache2/downloads/version.txt /usr/local/apache2/cgi-bin/
