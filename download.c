b7efa0edb477db46e2cc9ed0daca5661665de3d7
Ryosuke IHARA <124863136+RyoIHA@users.noreply.github.com>
2023-07-30 22:18:01 +0900

    Update

--- download.c
+++ download.c
@@ -1,146 +1,62 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <curl/curl.h>
-#include <archive.h>
-#include <archive_entry.h>
 
-#define REMOTE_URL "http://192.168.100.101/downloads/updatefile.tar.gz"
-#define LOCAL_PATH "/usr/local/apache2/downloads/updatefile.tar.gz"
-
-int extract_tar_gz(const char* filename) {
-    struct archive *a;
-    struct archive *ext;
-    struct archive_entry *entry;
-    int flags;
-    int r;
-
-    a = archive_read_new();
-    ext = archive_write_disk_new();
-    archive_write_disk_set_options(ext, ARCHIVE_EXTRACT_TIME);
-    archive_read_support_filter_all(a);
-    archive_read_support_format_all(a);
-    r = archive_read_open_filename(a, filename, 10240); // ファイルを開く
-    if (r != ARCHIVE_OK) {
-        return 1;
-    }
-    for (;;) {
-        r = archive_read_next_header(a, &entry);
-        if (r == ARCHIVE_EOF) {
-            break;
-        }
-        if (r != ARCHIVE_OK) {
-            return 1;
-        }
-        // ファイルを展開
-        r = archive_write_header(ext, entry);
-        if (r != ARCHIVE_OK) {
-            return 1;
-        }
-        for (;;) {
-            char buff[8192];
-            int size;
-            size = archive_read_data(a, buff, sizeof(buff));
-            if (size <= 0) {
-                break;
-            }
-            r = archive_write_data(ext, buff, size);
-            if (r != ARCHIVE_OK) {
-                return 1;
-            }
-        }
-        r = archive_write_finish_entry(ext);
-        if (r != ARCHIVE_OK) {
-            return 1;
-        }
-    }
-    archive_read_close(a);
-    archive_read_free(a);
-    archive_write_close(ext);
-    archive_write_free(ext);
-    return 0;
-}
-
-int main() {
-    CURL *curl;
-    CURLcode res;
-    FILE *fp;
-
+int main(void) {
+    // HTTPヘッダーを出力
     printf("Content-type: text/plain\n\n");
 
+    CURL *curl;
+    CURLcode res;
+    FILE *file;
+    FILE *logFile;
+    const char *download_path = "/usr/local/apache2/downloads/updatefile.tar.gz";
+    const char *url = "https://handsontiotapp.com/downloads/updatefile.tar.gz";
+    const char *log_path = "download_log.txt";
+
+    // libcurlの初期化
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
-    if (curl) {
-        fp = fopen(LOCAL_PATH, "wb");
-        if (fp == NULL) {
-            printf("Error opening file for writing: %s\n", LOCAL_PATH);
+
+    if(curl) {
+        // ファイルへの書き込みの準備
+        file = fopen(download_path, "wb");
+        if(!file) {
+            perror("Error opening file for writing");
             return 1;
         }
 
-        curl_easy_setopt(curl, CURLOPT_URL, REMOTE_URL);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-        curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
+        // ログファイルの準備
+        logFile = fopen(log_path, "a");
+        if(!logFile) {
+            perror("Error opening log file for writing");
+            fclose(file);
+            return 1;
+        }
 
-        // Disable SSL certificate verification
+        // URLの設定
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+
+        // サーバ証明書検証を無効にする
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 
-        res = curl_easy_perform(curl);
-        fclose(fp);
+        // ダウンロードデータをファイルに書き込む
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
 
-        if (res != CURLE_OK) {
-            printf("Download failed: %s\n", curl_easy_strerror(res));
-            curl_easy_cleanup(curl);
-            curl_global_cleanup();
-            return 1;
-        } else {
-            printf("Download succeeded!\n");
+        // ダウンロードの実行
+        res = curl_easy_perform(curl);
+
+        // エラー処理
+        if(res != CURLE_OK) {
+            fprintf(logFile, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         }
 
+        // リソースの解放
+        fclose(file);
+        fclose(logFile);
         curl_easy_cleanup(curl);
     }
 
     curl_global_cleanup();
-
-    // ダウンロードしたファイルを展開
-    int ret = extract_tar_gz(LOCAL_PATH);
-    if (ret != 0) {
-        printf("Extraction failed!\n");
-        return 1;
-    }
-
-    printf("Extraction succeeded!\n");
-
-    // update.shを実行
-    pid_t pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        return 1;
-    } else if (pid == 0) {
-        // 子プロセス
-        char *const argv[] = {"/bin/sh", "-c", "./update.sh", NULL};
-        execv("/bin/sh", argv);
-        perror("execv");
-        exit(1);
-    } else {
-        // 親プロセス
-        int status;
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status)) {
-            int exit_status = WEXITSTATUS(status);
-            if (exit_status == 0) {
-                printf("update.sh execution succeeded!\n");
-            } else {
-                printf("update.sh execution failed with exit status: %d\n", exit_status);
-                return 1;
-            }
-        } else {
-            printf("update.sh execution failed\n");
-            return 1;
-        }
-    }
-
     return 0;
 }
