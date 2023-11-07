32862aba06e97352ce6fe3c906c9909c394cbcad
Ryosuke IHARA <124863136+RyoIHA@users.noreply.github.com>
2023-11-07 13:01:39 +0900

    Update

--- Server/updatefile/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mv /usr/local/apache2/cgi-bin/updatefile/version.txt /usr/local/apache2/cgi-bin/
-
-
--- server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp bin/* /home/pi/cgi-bin
-cp bin/* /home/pi/backup
-cp htdocs/* /home/pi/backup
-cp version.txt /home/pi/cgi-bin
-cp version.txt /home/pi/backup
