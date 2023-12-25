09e8ba5e2041b8a60f6e3af59652e8433afa8144
Ryosuke IHARA <124863136+RyoIHA@users.noreply.github.com>
2023-12-25 15:52:05 +0900

    Update

--- network.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source setting.txt
-
-#ローカルIPアドレスの固定
-echo "interface $INTERFACE" | sudo tee -a /etc/dhcpcd.conf
-echo "static ip_address=$IP_ADDRESS/24" | sudo tee -a /etc/dhcpcd.conf
-echo "static routers=$ROUTERS" | sudo tee -a /etc/dhcpcd.conf
-echo "static domain_name_servers=${DNS//,/ }" | sudo tee -a /etc/dhcpcd.conf
