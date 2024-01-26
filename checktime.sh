b63ab1fd860d72c12f9736ec32ee5e677257ac44
Ryosuke IHARA <124863136+RyoIHA@users.noreply.github.com>
2024-01-26 18:43:31 +0900

    Update checktime.sh

--- checktime.sh
+++ checktime.sh
@@ -4,7 +4,7 @@
 start_time1=$(date +%s)
 # コマンド1を実行
 sudo sh apache.sh
-# コマンド1の終了時刻を記録し、実行時間を計算
+# コマンド1の終了時刻を記録し，実行時間を計算
 end_time1=$(date +%s)
 time1=$(($end_time1 - $start_time1))
 
@@ -12,10 +12,10 @@ time1=$(($end_time1 - $start_time1))
 start_time2=$(date +%s)
 # コマンド2を実行
 sudo sh webcam.sh
-# コマンド2の終了時刻を記録し、実行時間を計算
+# コマンド2の終了時刻を記録し，実行時間を計算
 end_time2=$(date +%s)
 time2=$(($end_time2 - $start_time2))
 
-# 2つのコマンドの実行時間を出力
-echo "Command 1 (apache.sh) took $time1 seconds."
-echo "Command 2 (webcam.sh) took $time2 seconds."
+# 2つのコマンドの実行時間をテキストファイルに出力
+echo "Command 1 (apache.sh) took $time1 seconds." >> command_times.txt
+echo "Command 2 (webcam.sh) took $time2 seconds." >> command_times.txt
